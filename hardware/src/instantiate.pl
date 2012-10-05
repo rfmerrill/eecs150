7d7ba05629850494777e3281d3d7bfeb1b433cd5
Robert F. Merrill <rfmerrill@berkeley.edu>
2012-10-05 07:38:01 -0700

    Added instantiate.pl and newmodule.py scripts

---
+++ hardware/src/instantiate.pl
@@ -0,0 +1,96 @@
+#!/usr/bin/env perl
+
+use strict;
+
+if ($#ARGV < 1) {
+  print "Usage: instantiate.pl <modulename> <instancename>\n";
+  exit(2);
+}
+
+my $modulename = shift(@ARGV);
+my $modulefilename = $modulename . ".v";
+my $instancename = shift(@ARGV);
+my $indentdepth = shift(@ARGV) || 0;
+
+
+open (my $modfile, "<", $modulefilename) || die "Can't open $modulefilename: $!";
+
+my @lines = <$modfile>;
+
+map { s{//.*}{}g } @lines;
+map { s{`.*}{}g } @lines;
+
+my $filestring = join " ", @lines;
+
+
+my $stripped = "";
+
+my $i = 0;
+my $j = 0;
+
+while (($i = index($filestring, "/*")) != -1) {
+  $stripped = $stripped . substr($filestring, 0, $i);
+
+  $filestring = substr($filestring, $i);
+
+  $j = index ($filestring, "*/");
+
+  if ($j == -1) { 
+    die "Comment not closed!";
+  }
+
+  $filestring = substr($filestring, $j + 2);
+}
+
+$stripped = $stripped . $filestring;
+     
+$stripped =~ s/;/ ; /g;
+$stripped =~ s/,/ , /g;
+$stripped =~ s/\(/ ( /g;
+$stripped =~ s/\)/ ) /g;
+$stripped =~ s/\s+/ /g;
+
+my @words = split(' ', $stripped);
+
+# print join("\n", @words);
+
+while (lc(shift(@words)) ne "module") {
+
+}
+
+my $foo = shift(@words);
+
+if (lc($foo) ne lc($modulename)) { die "$foo != $modulename"; }
+
+$foo = shift (@words);
+
+if ($foo ne "(")  { die "$foo is not an open paren"; }
+
+my @wires = ();
+
+while (($foo = shift (@words)) ne ")") {
+  next if (lc($foo) eq "wire");
+  next if (lc($foo) eq "reg");
+  next if (lc($foo) eq "input");
+  next if (lc($foo) eq "output");
+  next if ($foo eq ",");
+  next if ($foo eq ";");
+  next if ($foo =~ /^[^A-Za-z].*/);
+
+  push (@wires, $foo)
+
+}
+
+print "Wires: " . join(" ", @wires) . "\n";
+
+print $modulename . " " . $instancename . "(\n";
+
+my $wire = shift(@wires);
+print "  ." . $wire . "( )";
+
+while ($wire = shift(@wires)) {
+  print ",\n  ." . $wire . "( )";
+}
+print "\n);\n";
+
+ 
---
+++ hardware/src/newmodule.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+
+if len(sys.argv) < 2:
+  print 'No module name given'
+  sys.exit(2)
+
+modname = sys.argv[1]
+
+modfile = open (modname + '.v', 'w')
+
+modfile.write('module ' + modname + '(input clk, input rst);\n')
+modfile.write('  always @(*) begin\n\n')
+modfile.write('  end\n\n')
+modfile.write('  always @(posedge clk) begin\n\n')
+modfile.write('  end\n\n')
+modfile.write('endmodule\n')
+
+
