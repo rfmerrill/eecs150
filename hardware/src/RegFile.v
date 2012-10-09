0a552501666fa7799b6eea7f54213667607ad609
Robert F. Merrill <rfmerrill@berkeley.edu>
2012-10-08 22:42:35 -0700

    ARGHHHH why didn't I read the spec! That was like a whole day :(

--- hardware/src/RegFile.v
+++ hardware/src/RegFile.v
@@ -20,11 +20,11 @@ module RegFile(input clk,
                input  [31:0] wd,
                output [31:0] rd1, rd2);
 
-  reg [31:0] contents[0:31];
+  (* ram_style = "distributed" *) reg [31:0] contents[0:31];
 
   assign rd1 = (ra1 == 5'b0) ? 32'b0 : contents[ra1];
   assign rd2 = (ra2 == 5'b0) ? 32'b0 : contents[ra2];
-  
+    
   always @(posedge clk) begin
     if (we && (wa != 5'b0))
       contents[wa] <= wd;
