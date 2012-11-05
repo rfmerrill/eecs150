97f16efb55d92888e7cc5e00d50909b145c93a95
Robert F. Merrill <rfmerrill@berkeley.edu>
2012-11-04 18:36:53 -0800

    Final version for checkpoint 3

--- software/ISR/isr.s
+++ software/ISR/isr.s
@@ -48,7 +48,7 @@ beq   $k1, $0, inc_second_tens
 # store seconds
 li   $k1, 0x1000f104
 sb   $k0, 0($k1)
-j    done
+j    time_done
 
 inc_second_tens:
 li   $k1, 0x1000f104  #seconds
@@ -61,7 +61,7 @@ beq   $k1, $0, inc_minute
 # store second (tens digit)
 li    $k1, 0x1000f105
 sb    $k0, 0($k1)
-j     done
+j     time_done
 
 inc_minute:
 li   $k1, 0x1000f105  #seconds (tens)
@@ -74,7 +74,7 @@ beq   $k1, $0, inc_minute_tens
 # store minute
 li    $k1, 0x1000f106
 sb    $k0, 0($k1)
-j     done
+j     time_done
 
 
 inc_minute_tens:
@@ -88,12 +88,89 @@ sb  $k0, 0($k1)
 
 
 time_done: 
-#li   $k0, 0x100ff000    #load enable address
+li   $k0, 0x1000f300    #load enable address
 #addi $k1, $0, 0x1    #load 1 into $k1
-#bne  $k1, $k0, no_print  #check if enable is high
+lw   $k0, 0($k0)
+beq  $0, $k0, no_print  #check if enable is high
 
 
-no_print: #setting the compare register does this for us, not necessary.
+# print
+# 0x30 -- '0', 0x0d = CR, 0x3A = :
+
+#write the initial CR
+li   $k0, 0x1000f004  #load in_index
+lw   $k1, 0($k0)
+li   $k0, 0x1000ff00  #buffer address
+addu $k0, $k1, $k0    
+li   $k1, 0x0d
+sb   $k1, 0($k0)
+
+li   $k0, 0x1000f004  #load in_index
+lw   $k1, 0($k0)
+addiu $k1, $k1, 1
+andi $k1, $k1, 63
+li   $k0, 0x1000ff00  #buffer address
+addu $k0, $k1, $k0    #address to write to
+li   $k1, 0x1000f107  #minutes (tens)
+lbu  $k1, 0($k1)      
+addiu $k1, $k1, 0x30
+sb   $k1, 0($k0)
+
+li   $k0, 0x1000f004  #load in_index
+lw   $k1, 0($k0)
+addiu $k1, $k1, 2
+andi $k1, $k1, 63
+li   $k0, 0x1000ff00  #buffer address
+addu $k0, $k1, $k0    #address to write to
+li   $k1, 0x1000f106  #minutes (ones)
+lbu  $k1, 0($k1)      
+addiu $k1, $k1, 0x30
+sb   $k1, 0($k0)
+
+li   $k0, 0x1000f004  #load in_index
+lw   $k1, 0($k0)
+addiu $k1, $k1, 3
+andi $k1, $k1, 63
+li   $k0, 0x1000ff00  #buffer address
+addu $k0, $k1, $k0    #address to write to
+addiu $k1, $0, 0x3A   #colon
+sb   $k1, 0($k0)
+
+
+li   $k0, 0x1000f004  #load in_index
+lw   $k1, 0($k0)
+addiu $k1, $k1, 4
+andi $k1, $k1, 63
+li   $k0, 0x1000ff00  #buffer address
+addu $k0, $k1, $k0    #address to write to
+li   $k1, 0x1000f105  #seconds (tens)
+lbu  $k1, 0($k1)      
+addiu $k1, $k1, 0x30
+sb   $k1, 0($k0)
+
+li   $k0, 0x1000f004  #load in_index
+lw   $k1, 0($k0)
+addiu $k1, $k1, 5
+andi $k1, $k1, 63
+li   $k0, 0x1000ff00  #buffer address
+addu $k0, $k1, $k0    #address to write to
+li   $k1, 0x1000f104  #seconds (ones)
+lbu  $k1, 0($k1)      
+addiu $k1, $k1, 0x30
+sb   $k1, 0($k0)
+
+li  $k0, 0x1000f004
+lw  $k1, 0($k0)
+addiu $k1, $k1, 6
+andi $k1, $k1, 63
+sw  $k1, 0($k0)
+
+li  $k0, 0x80000000
+lw  $k0, 0($k0)
+bne $k0, $0, UARTTX_ISR
+
+no_print:
+#setting the compare register does this for us, not necessary.
 #mfc0 $k1, $13
 #li   $k0, 0xffff7fff
 #and  $k1, $k1, $k0
@@ -121,16 +198,33 @@ j done
 
 
 UARTRX_ISR:   # state = input character
-li  $k0, 0x8000000c    #data from receiever address
-lbu $k0, 0($k0)        #$k0 contains the value of data from receviver a word
-li  $k1, 0x1000f200    #load adress of State variable
-sb  $k0, 0($k1)
 mfc0 $k1, $13
 li   $k0, 0xfffffbff
 and  $k1, $k1, $k0
 mtc0 $k1, $13
+
+li  $k0, 0x8000000c    #data from receiever address
+lbu $k0, 0($k0)        #$k0 contains the value of data from receviver a word
+li  $k1, 0x65   # 'e'
+beq $k0, $k1, enable_time
+li  $k1, 0x64   # 'd'
+beq $k0, $k1, disable_time 
+
+li  $k1, 0x1000f200    #load adress of State variable
+sb  $k0, 0($k1)
+
 j done
 
+enable_time:
+li  $k0, 0x1
+li  $k1, 0x1000f300    #enable flag
+sw  $k0, 0($k1)
+j done
+
+disable_time:
+li  $k1, 0x1000f300
+sw  $0,  0($k1)
+j done
 
 
 UARTTX_ISR:
